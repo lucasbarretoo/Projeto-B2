f7e9b0cc3ec031a619eeed166beae1ff91e681b6
Lucas Barreto <olucas192@gmail.com>
2023-11-09 22:33:27 -0300

    Correções na comunicação da api NASA

--- app/Http/Controllers/Web/Nasa/NasaController.php
+++ app/Http/Controllers/Web/Nasa/NasaController.php
@@ -14,11 +14,14 @@ class NasaController extends Controller
             
             $response = Http::withHeaders([ 
                 'Accept'=> '*/*',
+            ])->withOptions([
+                'verify' => false
             ])->get('https://api.nasa.gov/planetary/apod?api_key=v1hq9QBJCJ0KMFnCL9soFRgPWfbluwHXhJyrkTC2'); 
             
             $NasaDetalhe = json_decode($response->body());
             return view('nasa.detalhe', compact('NasaDetalhe'));
         } catch (\Throwable $th) {
+            
             Session::put('classe', 'danger');
             Session::put('mensagem', 'Não foi possível realizar requisição API -Nasa!');
             return view('nasa.detalhe');
---
+++ tests/Unit/EscolaTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class EscolaTest extends TestCase
+{
+    /**
+     * A basic test Escola.
+     *
+     * @return void
+     */
+    public function test_escola_cadastro()
+    {
+        $this->assertTrue(true);
+    }
+}
